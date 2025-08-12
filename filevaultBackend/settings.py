acb70ba431fbbedd9e5e352665c47c81a5549a7a
shobhit99 <shobhitbhosure7@gmail.com>
2025-08-12 08:14:21 +0530

    allowing all hosts for now

--- filevaultBackend/settings.py
+++ filevaultBackend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-wa&xzb7$0&e#2=2p&het2=#7t9wk*4bb3t7-2+as^-%#$m9iye
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
